58bcdd1ee9d4dfc02a8c143e7f478b7a96f22547
aleja <aleja@LAPTOP-IMPQVJ86>
2024-04-15 09:23:41 +0200

    Petisuis supremacy

--- src/Holaa.java
+++ src/Holaa.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class Holaa {
 	
 	public static void main(String [] arg) {
 		
-		System.out.println("Hola guapo");
+		Scanner ent=new Scanner(System.in);
+		
+		
+		System.out.println("¿Cuantos rezos quieres realizar a nuestro dios?");
+		
+		int rezos=Integer.parseInt(ent.nextLine());
+		
+		for(int i=0; i <=rezos; i++)
+		System.out.println("Petisuis is love, Petisuis is live");
 	}
 
 }
